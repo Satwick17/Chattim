f12821e3f6527022c4fb919e3a2b48de9a65fe11
name <satwickdeep2001@gmail.com>
2025-08-14 00:38:17 +0530

    Sidebar

--- Client/store/useAuthStore.jsx
+++ Client/store/useAuthStore.jsx
@@ -8,6 +8,7 @@ export const useAuthStore = create((set) => ({
   isSigningUp: false,
   isUpdatingProfile: false,
   isCheckingAuth: true,
+  onlineUsers: [],
 
   checkAuth: async () => {
     try {
