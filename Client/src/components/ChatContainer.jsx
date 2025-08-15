5909fab1e7f418feaf82172ba10cf6acecdc6cd7
name <satwickdeep2001@gmail.com>
2025-08-15 23:49:21 +0530

    ChatContainer works

--- Client/src/components/ChatContainer.jsx
+++ Client/src/components/ChatContainer.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { use, useEffect } from "react";
+import { useChatStore } from "../../store/useChatStore";
+import MessageInput from "./MessageInput";
+import ChatHeader from "./ChatHeader";
 
 const ChatContainer = () => {
-  return <div>ChatContainer</div>;
+  const {messages, getMessages, isMessagesLoading, selectedUser} = useChatStore();
+
+  useEffect(()=>{
+    getMessages(selectedUser?._id);
+  }, [getMessages, selectedUser?._id])
+  
+
+  if(isMessagesLoading) return <div>Loading messages...</div>;
+  return <div className="flex-1 flex flex-col overflow-auto">
+    <ChatHeader/>
+
+    <p>Message....</p>
+
+    <MessageInput/>
+  </div>;
 };
 
 export default ChatContainer;
